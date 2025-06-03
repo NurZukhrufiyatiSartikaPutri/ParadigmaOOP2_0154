24b8a2e9eb5bad07a9eb0ff021938b79be63f437
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-03 19:23:59 +0700

    Menambahkan kelas 'orang' dengan konstruktor untuk inisialisasi umur

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,38 +1,47 @@
 #include <iostream>
-using namespace std;
+using namespace  std;
 
 class orang {
 public:
     int umur;
     orang(int pUmur) :
         umur(pUmur)
+        {
+            cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+        }
+};
+
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
     {
-        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+        cout << "pekerja dibuat\n" << endl;
     }
-        
 };
 
 class pelajar : virtual public orang {
 public:
 
-    pelajar(int pUmur):
+    pelajar(int pUmur) :
         orang(pUmur)
-    {
-        cout << "pelajar dibuat\n" << endl;
-    }
+        {
+            cout << "pelajar dibuat\n" << endl;
+        }
 };
 
 class budi : public pekerja, public pelajar {
 public:
 
-     budi(int pUmur) :
+    budi(int pUmur) :
         pekerja(pUmur),
         pelajar(pUmur),
-        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
-    {
-        cout << "Budi dibuat\n" << endl; 
-    }
+        orang(pUmur) // hal ini dapat dilakukan jika menggunakan virtual
+        {
+            cout << "Budi dibuat\n" << endl;
+        }
 };
+
 int main() {
     budi a(12);
 
